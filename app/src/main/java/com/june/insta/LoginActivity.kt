37e9b40c9fba04239306c7600e092a2c13aa8e7c
USER <tm.june.1974@gamil.com>
2021-08-23 14:10:50 +0900

    2 Google Login

--- app/src/main/java/com/june/insta/LoginActivity.kt
+++ app/src/main/java/com/june/insta/LoginActivity.kt
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +26,58 @@ class LoginActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+        //이메일 로그인
         email_login_button.setOnClickListener {
             signinAndSignup()
-        }
+        }//setOnClickListener
+
+        //SNS 로그인 1STEP
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }//setOnClickListener
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString((R.string.default_web_client_id))) //google api 키를 넣어서 토큰을 요청함
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
     }//onCreate
 
-    /* 로그인 플랫폼 사용하지 않고 로그인 */
+    /*SNS 로그인*/
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }//fun
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+
+            if (result.isSuccess) {
+                var account = result.signInAccount
+                //SNS 로그인 2STEP
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }//fun
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                //Login
+                moveMainPage(task.result?.user)
+            } else {
+                Log.d("err", "[ Login Failed_firebaseAuthWithGoogle ]")
+                Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }//fun
+
+    /* 이메일 로그인 */
     fun signinAndSignup() {
         auth?.createUserWithEmailAndPassword(
             email_edittext.text.toString(),
@@ -33,7 +86,7 @@ class LoginActivity : AppCompatActivity() {
             if (task.isSuccessful) {
                 moveMainPage(task.result?.user)
             } else if (task.exception?.message.isNullOrEmpty()) {
-                Log.d("insta", "[ Login Failed_1 ]")
+                Log.d("err", "[ Login Failed_signinAndSignup ]")
                 Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
             } else {
                 signinEmail()
@@ -50,17 +103,15 @@ class LoginActivity : AppCompatActivity() {
                 //Login
                 moveMainPage(task.result?.user)
             } else {
-                Log.d("insta", "[ Login Failed_2 ]")
+                Log.d("err", "[ Login Failed_signinEmail ]")
                 Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
             }
         }
     }//fun
 
     fun moveMainPage(user: FirebaseUser?) {
-        //파이어베이스에서 넘어온 유저 상태가 있을 경우, MainActivity 로 이동
         if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }//fun
-
-}
+}//class
