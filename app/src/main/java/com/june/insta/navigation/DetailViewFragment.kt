225ba2fdc5870a57002f42d2fb9575c9cfe26400
JUNE <june2ac4dev@gmail.com>
2021-08-29 15:43:08 +0900

    8 Like(2)

--- app/src/main/java/com/june/insta/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/june/insta/navigation/DetailViewFragment.kt
@@ -19,22 +19,20 @@ class DetailViewFragment : Fragment() {
     var firestore: FirebaseFirestore? = null
     var uid : String? = null
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
         firestore = FirebaseFirestore.getInstance()
         uid = FirebaseAuth.getInstance().currentUser?.uid
-
         view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
 
         return view
     }//onCreateView
 
+    //어댑터(Adapter) : 사용자 데이터 리스트로부터 아이템 뷰를 만들어 리사이클러뷰에 표시
     inner class DetailViewRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
         var contentUidList: ArrayList<String> = arrayListOf()
-
         init {
             firestore?.collection("images")?.orderBy("timestamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                     contentDTOs.clear()
@@ -48,25 +46,25 @@ class DetailViewFragment : Fragment() {
                 }
         }//init
 
+        //뷰홀더(ViewHolder) : 어댑터를 통해 만들어진 아이템 뷰를 저장. 어댑터에서 생성, 관리되고 재활용되어 자원을 효율적으로 사용함
+        //inner class CustomViewHolder,  override fun onCreateViewHolder 를 만든 것은 메모리를 적게 사용하기 위한 약속
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
             var view = LayoutInflater.from(parent.context).inflate(R.layout.item_detail, parent, false)
             return CustomViewHolder(view)
         }//onCreateViewHolder
 
-        //메모리를 적게 사용하기 위한 일종의 약속
-        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
+        //아이템 뷰에 들어갈 정보 배치
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
             var viewholder = (holder as CustomViewHolder).itemView
             viewholder.detailviewitem_profile_textview.text = contentDTOs!![position].userId
             Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewholder.detailviewitem_imageview_content)
             viewholder.detailviewitem_explain_textview.text = contentDTOs!![position].explain
             viewholder.detailviewitem_favoritecounter_textview.text = "Likes " + contentDTOs!![position].favoriteCount
-
             viewholder.detailviewitem_favorite_imageview.setOnClickListener {
                 favoriteEvent(position)
             }
-
             //좋아요가 눌렸을 때 하트색을 바꿔줌
             if(contentDTOs!![position].favorites.containsKey(uid)){
                 viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)
@@ -82,7 +80,6 @@ class DetailViewFragment : Fragment() {
         fun favoriteEvent(position : Int){
             var tsDoc = firestore?.collection("images")?.document(contentUidList[position])
             firestore?.runTransaction { transaction ->
-
                 var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
 
                 //containkey 가 되어있다는 것은 좋아요 버튼이 눌려있다는 것으로 버튼을 한번 더 누르면 좋아요가 취소됨
@@ -96,6 +93,6 @@ class DetailViewFragment : Fragment() {
                 //트랜잭션을 다시 서버로 돌려줌
                 transaction.set(tsDoc, contentDTO)
             }
-        }
+        }//favoriteEvent
     }//inner class
 }//class
