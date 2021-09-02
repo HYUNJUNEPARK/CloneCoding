393bdeb40d17fdfab7b44f386669db28af96c22e
JUNE <89306567+HYUNJUNEPARK@users.noreply.github.com>
2021-09-02 22:57:53 +0900

    11 Comment

--- app/src/main/java/com/june/insta/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/june/insta/navigation/DetailViewFragment.kt
@@ -1,5 +1,7 @@
 package com.june.insta.navigation
 
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -83,6 +85,13 @@ class DetailViewFragment : Fragment() {
                 bundle.putString("userId", contentDTOs[position].userId)
                 fragment.arguments = bundle
                 activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.main_content, fragment)?.commit()
+            }
+
+            //코멘트 이미지가 클릭됐을 때
+            viewholder.detailviewitem_comment_imageview.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[position])
+                startActivity(intent)
             }
         }//onBindViewHolder
 
