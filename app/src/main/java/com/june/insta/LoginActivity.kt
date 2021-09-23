ac98f5568fb68f0793861d704444305afe8f7df7
HYUNJUNEPARK <june2ac4dev@gmail.com>
2021-09-23 20:42:12 +0900

    16 Login

--- app/src/main/java/com/june/insta/LoginActivity.kt
+++ app/src/main/java/com/june/insta/LoginActivity.kt
@@ -1,9 +1,7 @@
 package com.june.insta
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -20,82 +18,94 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import kotlinx.android.synthetic.main.activity_login.*
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-import java.util.*
-
+import com.june.insta.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    var auth: FirebaseAuth? = null
-    var googleSignInClient: GoogleSignInClient?= null
-    var GOOGLE_LOGIN_CODE = 9001
-    var callbackManager: CallbackManager?= null
+    //[Variation For ViewBinding]
+    private lateinit var binding : ActivityLoginBinding
+
+    //[Variation For FirebaseAuth]
+    private var auth: FirebaseAuth? = null
+
+    //[Variation For Google Login]
+    private var googleSignInClient: GoogleSignInClient? = null
+    private var googleLoginCode = 9001
+    private var callbackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
 
-        //이메일 로그인
-        email_login_button.setOnClickListener {
-            signinAndSignup()
-        }
-
-        //SNS 로그인 1STEP
-        google_sign_in_button.setOnClickListener {
-            googleLogin()
-        }
-
-        facebook_login_button.setOnClickListener {
-            //1step
-            facebookLogin()
-        }
-
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString((R.string.default_web_client_id))) //google api 키를 넣어서 토큰을 요청함
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString((R.string.default_web_client_id)))
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
         callbackManager = CallbackManager.Factory.create()
-    }//onCreate
 
-//    //자동 로그인 기능
+        binding.emailLoginButton.setOnClickListener {
+            Log.d("checkLog","Email Login Button Clicked")
+            if(binding.emailEdittext.text.isNullOrEmpty()){
+                Toast.makeText(this, "ID/PW 를 확인해주세요", Toast.LENGTH_SHORT).show()
+            }else{
+                val id = binding.emailEdittext.text.toString()
+                val pw = binding.passwordEdittext.text.toString()
+
+                if(id.contains("@") && id.contains(".")){
+                    signIn(id, pw)
+                }else{
+                    Toast.makeText(this, "ID/PW 를 확인해주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.emailSignUpButton.setOnClickListener {
+            Log.d("checkLog","Email SignUp Button Clicked")
+            if(binding.emailEdittext.text.isNullOrEmpty()){
+                Toast.makeText(this.applicationContext, "ID/PW 를 확인해주세요", Toast.LENGTH_SHORT).show()
+            }else{
+                val id = binding.emailEdittext.text.toString()
+                val pw = binding.passwordEdittext.text.toString()
+
+                if(id.contains("@") && id.contains(".")){
+                    signUp(id, pw)
+                }else{
+                    Toast.makeText(this, "ID/PW 를 확인해주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.googleSignInButton.setOnClickListener {
+            Log.d("checkLog","Google Login Button Clicked")
+            googleLogin()
+        }
+
+        binding.facebookLoginButton.setOnClickListener {
+            Log.d("checkLog","Facebook Login Button Clicked")
+            facebookLogin()
+        }
+    }
+
+//    //[START 자동 로그인 기능]
 //    override fun onStart() {
 //        super.onStart()
 //        moveMainPage(auth?.currentUser)
-//    }//onStart
+//    }
+//    //[END 자동 로그인 기능]
 
-    /*1. 페이스북 로그인*/
-    //onCreate 에서 한번 호출해 로그로 찍힌 hashkey 를 얻는다
-    fun printHashKey() {
-        try {
-            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-            for (signature in info.signatures) {
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                val hashKey: String = String(Base64.encode(md.digest(), 0))
-                Log.i("log", "printHashKey() Hash Key: $hashKey")
-            }
-        } catch (e: NoSuchAlgorithmException) {
-            Log.e("log", "Can not print HashKey : NoSuchAlgorithmException", e)
-        } catch (e: Exception) {
-            Log.e("log", "Can not print HashKey", e)
-        }
-    }//printHashKey
 
-    fun facebookLogin(){
-        //페이스북에서 프로필과 이메일을 요청함
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
 
-        //페이스북 로그인에 성공했을 때 넘어오는 부분
+
+    //[START 페이스북 로그인]
+    private fun facebookLogin(){
+        LoginManager.getInstance().logInWithReadPermissions(this, listOf("public_profile", "email"))
+
         LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult>{
             override fun onSuccess(result: LoginResult?) {
-                //2step
-                //로그인 성공 시 정보를 파이어베이스에 넘김
                 handleFacebookAccessToken(result?.accessToken)
             }
             override fun onCancel() {
@@ -103,94 +113,93 @@ class LoginActivity : AppCompatActivity() {
             override fun onError(error: FacebookException?) {
             }
         })
-    }//facebookLogin
-
+    }
     fun handleFacebookAccessToken(token : AccessToken?){
-        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        val credential = FacebookAuthProvider.getCredential(token?.token!!)
         auth?.signInWithCredential(credential)?.addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                //3step
-                //Login
-                moveMainPage(task.result?.user)
+                Log.d("checkLog", "로그인 성공 MainActivity 오픈")
+                startActivity(Intent(this, MainActivity::class.java))
             } else {
-                Log.d("log", "[ Login Failed at handleFacebookAcessToken ]")
-                Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "로그인 오류 발생", Toast.LENGTH_SHORT).show()
             }
         }
-    }//handleFacebookAccessToken
+    }
+//    fun printHashKey() {
+//        try {
+//            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+//            for (signature in info.signatures) {
+//                val md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                val hashKey: String = String(Base64.encode(md.digest(), 0))
+//                Log.i("checkLog", "printHashKey() Hash Key: $hashKey")
+//            }
+//        } catch (e: NoSuchAlgorithmException) {
+//            Log.e("checkLog", "Can not print HashKey : NoSuchAlgorithmException", e)
+//        } catch (e: Exception) {
+//            Log.e("checkLog", "Can not print HashKey", e)
+//        }
+//    }
+    //[END 페이스북 로그인]
 
-
-    /*2. 구글 로그인*/
-    fun googleLogin() {
-        var signInIntent = googleSignInClient?.signInIntent
-        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
-    }//googleLogin
-
+    //[START 구글 로그인]
+    private fun googleLogin() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, googleLoginCode)
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        //onCreate 의 callbackManager 결과값이 onActivityResult 로 넘어옴
         callbackManager?.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == GOOGLE_LOGIN_CODE) {
-            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+        if (requestCode == googleLoginCode) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result.isSuccess) {
-                var account = result.signInAccount
-                //SNS 로그인 2STEP
+                val account = result.signInAccount
+                Log.d("checkLog", "파이어베이스권한 : " + account.id)
                 firebaseAuthWithGoogle(account)
+            } else {
+                Toast.makeText(this, "로그인 오류 발생", Toast.LENGTH_SHORT).show()
             }
         }
-    }//onActivityResult
-
-    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
-        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+    }
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
         auth?.signInWithCredential(credential)?.addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                //Login
-                moveMainPage(task.result?.user)
+                Log.d("checkLog", "로그인 성공 MainActivity 오픈")
+                startActivity(Intent(this, MainActivity::class.java))
             } else {
-                Log.d("log", "[ Login Failed at firebaseAuthWithGoogle ]")
-                Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "로그인 오류 발생", Toast.LENGTH_SHORT).show()
             }
         }
-    }//firebaseAuthWithGoogle
+    }
+    //[START 구글 로그인]
 
-    /*3. 이메일 로그인 */
-    fun signinAndSignup() {
-        auth?.createUserWithEmailAndPassword(
-            email_edittext.text.toString(),
-            password_edittext.text.toString()
-        )?.addOnCompleteListener { task ->
+    //[START 이메일 로그인]
+    private fun signIn(id:String, pw:String) {
+        auth?.signInWithEmailAndPassword(id, pw)?.addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                moveMainPage(task.result?.user)
-            } else if (task.exception?.message.isNullOrEmpty()) {
-                Log.d("log", "[ Login Failed at signinAndSignup ]")
-                Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
             } else {
-                signinEmail()
+                Toast.makeText(this, "로그인 오류 발생", Toast.LENGTH_SHORT).show()
             }
         }
-    }//signinAndSignup
-
-    fun signinEmail() {
-        auth?.signInWithEmailAndPassword(
-            email_edittext.text.toString(),
-            password_edittext.text.toString()
-        )?.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                //Login
-                moveMainPage(task.result?.user)
-            } else {
-                Log.d("log", "[ Login Failed at signinEmail ]")
-                Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+    }
+    private fun signUp(id:String, pw:String) {
+        auth?.createUserWithEmailAndPassword(id, pw)?.addOnCompleteListener { task ->
+            when {
+                task.isSuccessful -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                task.exception?.message.isNullOrEmpty() -> {
+                    Log.d("checkLog", "User Account Null or Empty")
+                    Toast.makeText(this, "로그인 오류 발생", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Toast.makeText(this, "로그인 오류 발생", Toast.LENGTH_SHORT).show()
+                }
             }
         }
-    }//signinEmail
-
-    fun moveMainPage(user: FirebaseUser?) {
-        if (user != null) {
-            startActivity(Intent(this, MainActivity::class.java))
-            //MainActivity 로 이동하면서 LoginActivity 를 닫음
-            finish()
-        }
-    }//moveMainPage
-}//class
+    }
+    //[END 이메일 로그인]
+}
--- app/src/main/java/com/june/insta/MainActivity.kt
+++ app/src/main/java/com/june/insta/MainActivity.kt
@@ -3,6 +3,7 @@ package com.june.insta
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,13 +18,14 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
+import com.june.insta.databinding.ActivityLoginBinding
 import com.june.insta.navigation.*
-import com.june.insta.navigation.util.FcmPush
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
-    // 네이게이션바
+
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         setToolbarDefault()
         when(item.itemId){
